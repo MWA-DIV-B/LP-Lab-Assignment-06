d322b39db0ac934d3d26ede41bca581bcd0896bd
Upendra Chandra Maurya <90821996+UpendraMaurya14@users.noreply.github.com>
2023-06-09 12:04:25 +0530

    Add files via upload

---
+++ CS3227/form.php
@@ -0,0 +1,33 @@
+<?php
+// MySQL database configuration
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// Prepare SQL statement
+$sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+
+// Execute SQL statement
+if ($conn->query($sql) === TRUE) {
+    echo "Registration successful!";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close the database connection
+$conn->close();
+?>
